08f9e489aa895870b2d4e4acaf09ed5c22a00244
Luke Lambert <Luke Lambert>
2016-09-01 13:47:41 +1000

    change 1st regexp

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const port = process.env.PORT || 8080;
 app.use('/api/whoami', (req, res) => {
   var software;
   try {
-    software = req.headers['user-agent'].match(/\([a-z\d.\s;#@&]*\)/i).toString().match(/[a-z\d.\s_;\-:#@&]*/i);
+    software = req.headers['user-agent'].match(/\([a-z\d.\s_;\-:#@&]*\)/i).toString().match(/[a-z\d.\s_;\-:#@&]*/i);
   } catch(err) {
     software = req.headers['user-agent'];
   }
   var language = req.headers['accept-language'].match(/^[a-z-]+/gi).toString();
 
   var obj = {
-    ipaddress: req.connection.remoteAddress,
+    ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
     language: language,
     software: software
   };
