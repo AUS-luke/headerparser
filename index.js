d885319decb8cd485ac7139a2dee5d3bcacfcf7b
Luke Lambert <Luke Lambert>
2016-09-01 13:25:22 +1000

    project update

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app.use('/api/whoami', (req, res) => {
+  var software = req.headers['user-agent'].match(/\([a-z\d.\s;#@&]*\)/i).toString().replace(/[{()}]/g, '');
+  var language = req.headers['accept-language'].match(/^[a-z-]+/gi).toString();
+
+  var obj = {
+    ipaddress: req.connection.remoteAddress,
+    language: language,
+    software: software
+  };
+
+  res.send(obj);
+  res.end();
+
+
+});
+
+app.listen(port);
