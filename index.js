64e63f9e98931431ff0b62cfad655ec67508c546
Luke Lambert <Luke Lambert>
2016-09-01 13:41:40 +1000

    added _:- to regexp

--- index.js
+++ index.js
@@ -5,22 +5,17 @@ const port = process.env.PORT || 8080;
 
 
 app.use('/api/whoami', (req, res) => {
-  console.log(['user-agent']);
-  var software = req.headers['user-agent'];
+  var software = req.headers['user-agent'].match(/\([a-z\d.\s;#@&]*\)/i).toString().match(/[a-z\d.\s_;\-:#@&]*/i);
   var language = req.headers['accept-language'].match(/^[a-z-]+/gi).toString();
 
   var obj = {
-    ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    ipaddress: req.connection.remoteAddress,
     language: language,
     software: software
   };
-
   res.send(obj);
   res.end();
-
-
 });
 
-app.listen(port);
 
-//.match(/\([a-z\d.\s;#@&]*\)/i).toString().replace(/[{()}]/g, '');
+app.listen(port);
